819e4ec972f64b8f0cc3f95369db18b112899792
Luiginoo0 <luigiromano417@gmail.com>
2026-03-28 18:41:56 +0100

    modifica

--- core/motore_verdetto.py
+++ core/motore_verdetto.py
@@ -4,7 +4,7 @@ from groq import Groq
 from dotenv import load_dotenv
 
 load_dotenv()
-client = Groq(api_key=os.getenv("GROK_API_KEY"))
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def genera_verdetto_probabilistico(claim, dossier_arricchito):
     """
